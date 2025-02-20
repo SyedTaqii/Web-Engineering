8e0922e8c2558859e905abd132320e85e6e1eec3
SyedTaqii <taqisyed141@gmail.com>
2025-02-20 13:15:42 +0500

    promis, callback, localStorage, fetch api, JSON, stringify

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,32 +1,54 @@
-// let fruits = ["Apple", "Banana", "Orange"];
+// let fruits = ["Apple", "Banana", "Orange"]; 
 let fruits = new Array("Apple", "Banana", "Orange", "Apricot", "Guava");
 
-console.log(fruits);
-// fruits.splice(1, 1, "Apple");
-// fruits.splice(2, 4, "Apple", "Orange", "Apricot", "Guava");
+console.log(fruits); 
+// Output: [ 'Apple', 'Banana', 'Orange', 'Apricot', 'Guava' ]
 
-// fruits.slice(0, 1);
-fruits.indexOf("Apricot");
-console.log(fruits.indexOf("Apricot")); //return -1 meaning the thing does not exist
+fruits.splice(1, 1, "Apple");
+console.log(fruits);
+// Output: [ 'Apple', 'Apple', 'Orange', 'Apricot', 'Guava' ]
+
+fruits.splice(2, 4, "Apple", "Orange", "Apricot", "Guava");
+console.log(fruits);
+// Output: [ 'Apple', 'Apple', 'Apple', 'Orange', 'Apricot', 'Guava' ]
+
+// fruits.slice(0, 1); 
+console.log(fruits.indexOf("Apricot")); 
+// Output: 4 (index of "Apricot" in the modified array)
 
 fruits.forEach((value) => console.log(value.toUpperCase())); //callback loop function
-console.log(fruits.filter(val => val === "Apple"));
+// Output: 
+// APPLE
+// APPLE
+// APPLE
+// ORANGE
+// APRICOT
+// GUAVA
+
+console.log(fruits.filter(val => val === "Apple")); 
+// Output: [ 'Apple', 'Apple', 'Apple' ]
 
 // let htmlListItems = fruits.map(val => "<li>" + val + "</li>");
 // console.log(htmlListItems);
+// Output: [ '<li>Apple</li>', '<li>Apple</li>', '<li>Apple</li>', '<li>Orange</li>', '<li>Apricot</li>', '<li>Guava</li>' ]
 
-let value = [1,2,5,8,9];
+let value = [1, 2, 5, 8, 9];
 console.log((value.reduce(((result, val) => result * val), 0)));
+// Output: 0 (because the initial value is 0, multiplying by anything stays 0)
 
 // let result = fruits.reduce((result, val) => result + "<li>" + val + "</li>"), "<ul>");
-// result+="</ul>";
+// result += "</ul>";
 
 let resultMax = value.reduce(((result, val) => result > val ? result : val), value[0]);
 console.log(resultMax);
+// Output: 9 (maximum value in the array)
 
 let valueSet = new Set([1, 3, 5, 1]);
 console.log(valueSet);
+// Output: Set { 1, 3, 5 } (duplicates are removed in a Set)
 
-function hello(){
+function hello() {
     console.log("hello");
 }
+hello();
+// Output: hello
--- weLec9/script.js
+++ weLec9/script.js
@@ -1,7 +1,121 @@
-console,console.log("I am Initial");
+// console,console.log("I am Initial");
 
-setTimeout(function(){
-    console.log("I am Inside");
-}, 2000);  
+// setTimeout(function(){
+//     console.log("I am Inside");
+// }, 2000);  
 
-console.log("I am Ending");
+// console.log("I am Ending");
+
+// console.log("Before");
+
+// function fetchUser(){
+//     setTimeout(function(){
+//         console.log("I am Inside");
+//         return {name: "Asad", age: 20};
+//     }, 2000);
+// }
+
+// const returnedUser = fetchUser();
+// console.log(returnedUser);
+
+//callback function
+
+// fetchUser(123, function(user){
+//     console.log(user);
+// });
+
+// function fetchUser(userId, callback){
+//     setTimeout(function(){
+//         const fetchedUser = {id: userId, name: "Asad", email: "asad@123", age: 20};
+//         callback(fetchedUser);
+//     }, 2000);
+// }
+
+// function sendEmail(userEmail, callback){
+//     setTimeout(function(){
+//         const response = {status: true};
+//         callback(response);
+//     }, 3000);
+// }
+
+// sendEmail("asad@123", function(response){
+//     console.log(response);
+// });
+
+// const promise = new Promise(function(resolve, reject){
+//     setTimeout(function(){
+//         resolve(1);
+//         reject(new Error("I am Rejected"));
+//     }, 2000);
+// });
+
+// promise.then(function(result){ //resolve will call this function
+//     console.log(result);
+// });
+
+// promise.catch(function(error){ //reject will call this function
+//     console.log(error.message);
+// });
+
+// const promise = new Promise(function(resolve, reject){
+//     resolve(1);
+//     reject(new Error("I am Rejected"));
+// });
+
+// Promise.all([promise])
+//     .then(function(result){
+//         console.log("working" + result);
+//     })
+//     .catch(function(error){
+//         console.log("not working"+error.message);
+//     });
+
+// console.log("End");
+
+
+// async function getGitData(){
+//     const response = await fetch("https://api.github.com/users/SyedTaqii");
+//     const profile = await response.json();
+//     console.log(profile);
+// }
+// getGitData();
+// const getAPI = fetch("https://api.github.com/users/hashimaliii");
+
+// getAPI.then((response) => {
+//     response.json();
+// })
+// .then(function(profile){
+//     console.log(profile);
+// });
+
+// getAPI.catch(function(error){    
+//     console.log(error);
+// });
+
+// localStorage.setItem('gitURL', 'api.github.com/users/hashimaliii');
+
+const users = [
+    {name: "Asad", age: 20},
+    {name: "Ali", age: 22},
+    {name: "Taqi", age: 24}
+];
+
+console.log(users);
+
+sampleusers = JSON.stringify(users); // convert object to string
+console.log(sampleusers);
+console.log(JSON.parse(sampleusers)); // convert string to object
+
+localStorage.setItem('classStudents', sampleusers);
+console.log(localStorage.getItem('classStudents'));
+
+//Task 
+//Record Expenses
+//Source: input
+//amount: input
+//spend and income button
+
+//debit balance (display)    //Credit Balance(display)
+//green and +                 //red and -
+//total amount (display)
+//history
